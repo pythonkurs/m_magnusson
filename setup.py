60972190330983c3f66fdc6174c0032c6d5d4333
Mans Magnusson <monsunas@gmail.com>
2013-01-23 11:34:11 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='biostuff', 
+	version='0.1',
+    description="Python Course example package",
+	author='Mans Magnusson', 
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	author_email='mans.magnusson@scilifelab.e',
+	url='',
+	packages=['biostuff'],
+	)
